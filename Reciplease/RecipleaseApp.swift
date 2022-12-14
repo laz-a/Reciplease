e6d613754a6849461c400dba3bac5902b9323f0e
laz <laz@laz.com>
2022-12-14 22:15:51 +0100

    Initial Commit

---
+++ Reciplease/RecipleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipleaseApp.swift
+//  Reciplease
+//
+//  Created by laz on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
