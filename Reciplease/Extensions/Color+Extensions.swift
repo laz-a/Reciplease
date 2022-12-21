42f60fd236b8c3f91a4b9503186e50368e58f5f0
laz <preperfect@gmail.com>
2022-12-21 15:12:50 +0100

    New color

---
+++ Reciplease/Extensions/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  Reciplease
+//
+//  Created by laz on 20/12/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let darkBackground = Color(red: 54 / 255, green: 50 / 255, blue: 49 / 255, opacity: 1)
+    static let greenButton = Color(red: 67 / 255, green: 147 / 255, blue: 93 / 255, opacity: 1)
+    static let grayButton = Color(red: 138 / 255, green: 138 / 255, blue: 138 / 255, opacity: 1)
+}
