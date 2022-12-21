b48e9ff1424a9a3867606306fee67864f9084446
laz <preperfect@gmail.com>
2022-12-21 15:21:16 +0100

    Data test

---
+++ RecipleaseTests/ModelData.swift
@@ -0,0 +1,37 @@
+//
+//  modelData.swift
+//  Reciplease
+//
+//  Created by laz on 18/12/2022.
+//
+
+import Foundation
+import Combine
+
+final class ModelData: ObservableObject {
+    var recipes: [Recipe] = load("recipeData.json")
+}
+
+
+func load(_ filename: String) -> [Recipe] {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        let edamam = try decoder.decode(Edamam.self, from: data)
+        return edamam.recipes
+    } catch {
+        fatalError("Couldn't parse \(filename) as \([Recipe].self):\n\(error)")
+    }
+}
